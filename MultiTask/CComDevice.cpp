f8c434b3326dfbffb217e20fc59ef62399235f9a
Kobayashi <masato.kobayashi@shi-g.com>
2019-12-07 17:12:27 +0900

    update20191207

--- MultiTask/CComDevice.cpp
+++ MultiTask/CComDevice.cpp
@@ -1,11 +1,290 @@
 #include "stdafx.h"
 #include "CComDevice.h"
 
-CComDevice::CComDevice()
-{
-}
+CComDevice::CComDevice(){}
+CComDevice::~CComDevice(){}
+
+void CComDevice::init_task(void *pobj) {
+	set_panel_tip_txt();
+	return;
+};
+
+void CComDevice::routine_work(void *param) {
+	
+	set_RIO_incl();
+
+	double incl_x = get_RIO_incl(COMD_INCL_MODE_RAD, COMD_INCL_AXIS_X);
+	double incl_y = get_RIO_incl(COMD_INCL_MODE_RAD, COMD_INCL_AXIS_Y);
+	
+	ws  << L" X(rad): "<< incl_x << L"   Y(rad): "<< incl_y << L" working!" << *(inf.psys_counter)%100;
+	tweet2owner(ws.str()); ws.str(L""); ws.clear();
+
+};
+
+LRESULT CALLBACK CComDevice::PanelProc(HWND hDlg, UINT msg, WPARAM wp, LPARAM lp) {
+	switch (msg) {
+
+	case WM_COMMAND:
+		switch (LOWORD(wp)) {
+		case IDC_TASK_FUNC_RADIO1:
+		case IDC_TASK_FUNC_RADIO2:
+		case IDC_TASK_FUNC_RADIO3:
+		case IDC_TASK_FUNC_RADIO4:
+		case IDC_TASK_FUNC_RADIO5:
+		case IDC_TASK_FUNC_RADIO6:
+			inf.panel_func_id = LOWORD(wp); set_panel_tip_txt(); set_PNLparam_value(0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+			break;
+
+		case IDC_TASK_ITEM_RADIO1:
+		case IDC_TASK_ITEM_RADIO2:
+		case IDC_TASK_ITEM_RADIO3:
+		case IDC_TASK_ITEM_RADIO4:
+		case IDC_TASK_ITEM_RADIO5:
+		case IDC_TASK_ITEM_RADIO6:
+			inf.panel_type_id = LOWORD(wp); set_panel_tip_txt();  SetFocus(GetDlgItem(inf.hWnd_opepane, IDC_TASK_EDIT1));
+			break;
+
+		case IDSET: {
+			wstring wstr, wstr_tmp;
+
+			//ƒTƒ“ƒvƒ‹‚Æ‚µ‚Ä‚¢‚ë‚¢‚ë‚ÈŒ^‚Å“Ç‚Ýž‚ñ‚Å•\Ž¦‚µ‚Ä‚¢‚é
+			wstr += L"Param 1(d):";
+			int n = GetDlgItemText(hDlg, IDC_TASK_EDIT1, (LPTSTR)wstr_tmp.c_str(), 128);
+			if (n) wstr_tmp = to_wstring(stod(wstr_tmp));	wstr = wstr + wstr_tmp.c_str();
+
+			wstr += L",  Param 2(i):";
+			n = GetDlgItemText(hDlg, IDC_TASK_EDIT2, (LPTSTR)wstr_tmp.c_str(), 128);
+			if (n) wstr_tmp = to_wstring(stoi(wstr_tmp));	wstr = wstr + wstr_tmp.c_str();
+
+			wstr += L",  Param 3(f):";
+			n = GetDlgItemText(hDlg, IDC_TASK_EDIT3, (LPTSTR)wstr_tmp.c_str(), 128);
+			if (n) wstr_tmp = to_wstring(stof(wstr_tmp));	wstr = wstr + wstr_tmp.c_str();
+
+			wstr += L",  Param 4(l):";
+			n = GetDlgItemText(hDlg, IDC_TASK_EDIT4, (LPTSTR)wstr_tmp.c_str(), 128);
+			if (n) wstr_tmp = to_wstring(stol(wstr_tmp));	wstr = wstr + wstr_tmp.c_str();
+
+			wstr += L",  Param 5(c):";
+			n = GetDlgItemText(hDlg, IDC_TASK_EDIT5, (LPTSTR)wstr_tmp.c_str(), 128);
+			wstr += wstr_tmp.c_str();
+
+			wstr += L",   Param 6(c):";
+			n = GetDlgItemText(hDlg, IDC_TASK_EDIT6, (LPTSTR)wstr_tmp.c_str(), 128);
+			wstr += wstr_tmp.c_str();
+
+			txout2msg_listbox(wstr);
 
 
-CComDevice::~CComDevice()
+		}break;
+		case IDRESET: {
+			set_PNLparam_value(0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+
+		}break;
+
+		case IDC_TASK_OPTION_CHECK1: {
+			SendMessage(GetDlgItem(hDlg, IDC_TASK_OPTION_CHECK2), BM_SETCHECK, BST_UNCHECKED, 0L);
+			if (IsDlgButtonChecked(hDlg, IDC_TASK_OPTION_CHECK1) == BST_CHECKED) inf.work_select = THREAD_WORK_OPTION1;
+			else inf.work_select = THREAD_WORK_ROUTINE;
+		}
+			break;
+		case IDC_TASK_OPTION_CHECK2: {
+			SendMessage(GetDlgItem(hDlg, IDC_TASK_OPTION_CHECK1), BM_SETCHECK, BST_UNCHECKED, 0L);
+			if (IsDlgButtonChecked(hDlg, IDC_TASK_OPTION_CHECK2) == BST_CHECKED) inf.work_select = THREAD_WORK_OPTION2;
+			else inf.work_select = THREAD_WORK_ROUTINE;
+		}
+			break;
+		}
+		break;
+	}
+	return 0;
+};
+
+void CComDevice::set_panel_tip_txt()
 {
+	wstring wstr_type; wstring wstr;
+	switch (inf.panel_func_id) {
+	case IDC_TASK_FUNC_RADIO1: {
+		wstr = L"Type for Func1 \n\r 1:?? 2:?? 3:?? \n\r 4:?? 5:?? 6:??";
+		switch (inf.panel_type_id) {
+		case IDC_TASK_ITEM_RADIO1:
+			wstr_type += L"Param of type1 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		case IDC_TASK_ITEM_RADIO2:
+			wstr_type += L"Param of type2 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		case IDC_TASK_ITEM_RADIO3:
+			wstr_type += L"Param of type3 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		case IDC_TASK_ITEM_RADIO4:
+			wstr_type += L"Param of type4 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		case IDC_TASK_ITEM_RADIO5:
+			wstr_type += L"Param of type5 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		case IDC_TASK_ITEM_RADIO6:
+			wstr_type += L"Param of type6 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		default:break;
+		}
+	}break;
+	case IDC_TASK_FUNC_RADIO2: {
+		wstr = L"Type of Func2 \n\r 1:?? 2:?? 3:?? \n\r 4:?? 5:?? 6:??";
+		switch (inf.panel_type_id) {
+		case IDC_TASK_ITEM_RADIO1:
+			wstr_type += L"Param of type1 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		case IDC_TASK_ITEM_RADIO2:
+			wstr_type += L"Param of type2 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		case IDC_TASK_ITEM_RADIO3:
+			wstr_type += L"Param of type3 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		case IDC_TASK_ITEM_RADIO4:
+			wstr_type += L"Param of type4 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		case IDC_TASK_ITEM_RADIO5:
+			wstr_type += L"Param of type5 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		case IDC_TASK_ITEM_RADIO6:
+			wstr_type += L"Param of type6 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		default:break;
+		}
+	}break;
+	case IDC_TASK_FUNC_RADIO3: {
+		wstr = L"Type for Func3 \n\r 1:?? 2:?? 3:?? \n\r 4:?? 5:?? 6:??";
+		switch (inf.panel_type_id) {
+		case IDC_TASK_ITEM_RADIO1:
+			wstr_type += L"Param of type1 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		case IDC_TASK_ITEM_RADIO2:
+			wstr_type += L"Param of type2 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		case IDC_TASK_ITEM_RADIO3:
+			wstr_type += L"Param of type3 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		case IDC_TASK_ITEM_RADIO4:
+			wstr_type += L"Param of type4 \n\r 1:AS_ACT 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		case IDC_TASK_ITEM_RADIO5:
+			wstr_type += L"Param of type5 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		case IDC_TASK_ITEM_RADIO6:
+			wstr_type += L"Param of type6 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		default:break;
+		}
+	}break;
+	case IDC_TASK_FUNC_RADIO4: {
+		wstr = L"Type for Func4 \n\r 1:?? 2:?? 3:?? \n\r 4:?? 5:?? 6:??";
+		switch (inf.panel_type_id) {
+		case IDC_TASK_ITEM_RADIO1:
+			wstr_type += L"Param of type1 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		case IDC_TASK_ITEM_RADIO2:
+			wstr_type += L"Param of type2 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		case IDC_TASK_ITEM_RADIO3:
+			wstr_type += L"Param of type3 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		case IDC_TASK_ITEM_RADIO4:
+			wstr_type += L"Param of type4 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		case IDC_TASK_ITEM_RADIO5:
+			wstr_type += L"Param of type5 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		case IDC_TASK_ITEM_RADIO6:
+			wstr_type += L"Param of type6 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		default:break;
+		}
+	}break;
+	case IDC_TASK_FUNC_RADIO5: {
+		wstr = L"Type for Func5 \n\r 1:?? 2:?? 3:?? \n\r 4:?? 5:?? 6:??";
+		switch (inf.panel_type_id) {
+		case IDC_TASK_ITEM_RADIO1:
+			wstr_type += L"Param of type1 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		case IDC_TASK_ITEM_RADIO2:
+			wstr_type += L"Param of type2 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		case IDC_TASK_ITEM_RADIO3:
+			wstr_type += L"Param of type3 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		case IDC_TASK_ITEM_RADIO4:
+			wstr_type += L"Param of type4 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		case IDC_TASK_ITEM_RADIO5:
+			wstr_type += L"Param of type5 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		case IDC_TASK_ITEM_RADIO6:
+			wstr_type += L"Param of type6 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		default:break;
+		}
+	}break;
+	case IDC_TASK_FUNC_RADIO6: {
+		wstr = L"Func6(Mode) \n\r 1:?? 2:?? 3:?? \n\r 4:?? 5:?? 6:??";
+		switch (inf.panel_type_id) {
+		case IDC_TASK_ITEM_RADIO1:
+			wstr_type += L"Param of type1 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		case IDC_TASK_ITEM_RADIO2:
+			wstr_type += L"Param of type2 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		case IDC_TASK_ITEM_RADIO3:
+			wstr_type += L"Param of type3 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		case IDC_TASK_ITEM_RADIO4:
+			wstr_type += L"Param of type4 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		case IDC_TASK_ITEM_RADIO5:
+			wstr_type += L"Param of type5 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		case IDC_TASK_ITEM_RADIO6:
+			wstr_type += L"Param of type6 \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+			break;
+		default:break;
+		}
+	}break;
+	default: {
+		wstr = L"Type for Func? \n\r 1:?? 2:?? 3:?? \n\r 4:?? 5:?? 6:??";
+		wstr_type += L"(Param of type?) \n\r 1:?? 2:??  3:?? \n\r 4:?? 5:?? 6:??";
+	}break;
+	}
+
+	SetWindowText(GetDlgItem(inf.hWnd_opepane, IDC_STATIC_TASKSET3), wstr.c_str());
+	SetWindowText(GetDlgItem(inf.hWnd_opepane, IDC_STATIC_TASKSET4), wstr_type.c_str());
 }
+
+double CComDevice::get_RIO_incl(int mode, int axis) {
+	if (axis == COMD_INCL_AXIS_X) {
+		if (COMD_INCL_MODE_RAD) {
+			return s_RIO.inclination_rad_x;
+		}
+		else if (COMD_INCL_MODE_DEG) {
+			return s_RIO.inclination_deg_x;
+		}
+		else return 0.0;
+	}
+	else if (axis == COMD_INCL_AXIS_Y) {
+		if (COMD_INCL_MODE_RAD) {
+			return s_RIO.inclination_rad_y;
+		}
+		else if (COMD_INCL_MODE_DEG) {
+			return s_RIO.inclination_deg_y;
+		}
+		else return 0.0;
+	}
+};
+int CComDevice::set_RIO_incl() {
+	s_RIO.inclination_rad_x += 3.14/180.0;
+	if(s_RIO.inclination_rad_x > 3.14/180 * 30.0){
+		s_RIO.inclination_rad_x = -1.0 * 3.14 / 180 * 30.0;
+	}
+	s_RIO.inclination_rad_y -= 3.14 / 180.0;
+	if (s_RIO.inclination_rad_y < -1.0 * 3.14 / 180 * 30.0) {
+		s_RIO.inclination_rad_y = 3.14 / 180 * 30.0;
+	}
+	return 0;
+};
--- MultiTask/CComDevice.h
+++ MultiTask/CComDevice.h
@@ -5,11 +5,38 @@
 extern vector<void*>	VectpCTaskObj;	//タスクオブジェクトのポインタ
 extern ST_iTask g_itask;
 
+
+#define COMD_INCL_AXIS_X	1
+#define COMD_INCL_AXIS_Y	2
+#define COMD_INCL_MODE_RAD	1
+#define COMD_INCL_MODE_DEG	2
+
+
+typedef struct _stRIO {
+	DWORD error_status;
+	double inclination_rad_x;
+	double inclination_rad_y;
+	double inclination_deg_x;
+	double inclination_deg_y;
+}ST_RIO, *LPST_RIO;
+
+
 class CComDevice :
 	public CTaskObj
 {
 public:
 	CComDevice();
 	~CComDevice();
+	
+	ST_RIO s_RIO;
+
+	void routine_work(void *param);
+	void init_task(void *pobj);
+
+	LRESULT CALLBACK PanelProc(HWND hDlg, UINT msg, WPARAM wp, LPARAM lp);
+	void set_panel_tip_txt();//タブパネルのStaticテキストを設定
+		
+	int set_RIO_incl();
+	double get_RIO_incl(int mode, int axis);
 };
 
